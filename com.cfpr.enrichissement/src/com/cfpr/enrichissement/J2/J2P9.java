8afbc3f3086742fdc31eba588a121f1b78fb2f98
a0501809 <a0501809@TP-401-0037327.info.csaffluents.qc.ca>
2023-11-07 16:21:13 -0500

    idk

--- com.cfpr.enrichissement/src/com/cfpr/enrichissement/J2/J2P9.java
+++ com.cfpr.enrichissement/src/com/cfpr/enrichissement/J2/J2P9.java
@@ -1,10 +1,39 @@
 package com.cfpr.enrichissement.J2;
 
+import java.util.Scanner;
+
 public class J2P9 {
 
+	
+	static Scanner lectureClavier = new Scanner(System.in);
+	static public final String MSG_DEMANDE = "Entrez un nombre: ";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double rayon,circonference;
+		
+		rayon = saisirClavier();
+		circonference = calculerCirconference(rayon);
+	}
+	
+	public static double calculerCirconference(double rayon) {
+		double circonference;
+		
+		circonference= 2*rayon*
+		
+		return circonference;
 	}
 
+	public static double saisirClavier() {
+		double rayon = 0;
+		
+		afficherMessage(MSG_DEMANDE);
+		
+		rayon = lectureClavier.nextDouble();
+		
+		return rayon;
+	}
+	
+	public static void afficherMessage(String message) {
+		System.out.println(message);
+	}
 }
